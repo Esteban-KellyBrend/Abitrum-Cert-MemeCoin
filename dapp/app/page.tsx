17ffcf82b5e7b2c2554deafe7f1344741407228b
Esteban-KellyBrend <estebankellybrend@gmail.com>
2024-03-01 10:07:42 +0800

    page changes

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -100,26 +100,46 @@ return (
       backgroundImage: `url('https://www.dexerto.com/cdn-image/wp-content/uploads/2024/02/27/mrfresh-cat-bounty-outrage.jpg?width=828&quality=75&format=auto')`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',}}>
+
+        <div style={{ 
+      border: '2px solid black', 
+      padding: '40px', 
+      margin: '20px', 
+      backgroundColor: 'maroon',
+      backgroundSize: 'cover',
+      backgroundImage: `url('https://us-tuna-sounds-images.voicemod.net/83cf4323-16bb-4aca-895e-019c1d748381-1681607929918.jpg')`
+    }}>
           <button onClick={() => {connectWallet();}}
-        className="p-3 bg-slate-800 text-white rounded"
+        className="p-3 bg-yellow-400 text-white rounded"
       >
         {walletKey != "" ? walletKey : " Connect wallet"}
       </button>
+      </div>
 
-      <div>
+      <div style={{ 
+      border: '2px solid black', 
+      padding: '40px', 
+      margin: '20px', 
+      backgroundColor: 'lightgray',
+      textAlign: 'center',
+      backgroundImage: `url('https://tr.rbxcdn.com/40e5bbcc6c67c44166f7fa425025d896/420/420/Hat/Png')`
+
+    }}>
+      <div >
       <br></br>
       <form>
-        <label> Input Amount To Mint</label><br></br>
+      <div style={{ textAlign: 'left' }}>Mint Amount</div>
         </form>
       <input
         type="number"
         value = {mintingAmount || ""}
         onChange = {(e) => mintAmountChange(e)}
-        style={{color:"Black"}}
+        style={{color:"Black", marginRight: '10px' }}
       />
       <button 
         onClick={() => {mintCoin();}}
         className="p-3 bg-slate-800 text-white rounded"
+        style={{ width: '120px', background: 'linear-gradient(to right, #4F46E5, #945DD6)' }}
       >
         {"Mint Token"}
       </button> 
@@ -129,20 +149,21 @@ return (
 
 <div>
 <form>
-    <label> Input Amount To Stake</label><br></br>
+<div style={{ textAlign: 'left' }}>Stake Amount</div>
     </form>
   <input
     type="number"
     value = {stakingAmount || ""}
     onChange = {(e) => stakeAmountChange(e)}
-    style={{color:"Black"}}
+    style={{color:"Black", marginRight: '10px' }}
   />
  
   <button 
     onClick={stakeCoin}
     className="p-3 bg-slate-800 text-white rounded"
+    style={{ width: '120px', background: 'linear-gradient(to right, #4F46E5, #945DD6)'}}
   >
-    {"Stake It"}
+    {"Stake Token"}
   </button> 
 </div>
 
@@ -151,10 +172,12 @@ return (
     <br></br>
     <button 
         onClick={withdrawCoin}
-        className="p-3 bg-slate-800 text-white rounded"
+        className="p-3 bg-slate-800 text-black rounded"
+        style={{ width: '120px', background: 'linear-gradient(to right, #FFA500, #FFFFFF)'}}
       >
         {"Withdraw"}
       </button> 
+      </div>
       </div>
 
     </main>
